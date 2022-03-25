a65ef09a5884a0cf98a6469e95b3eabeb6947e42
Gonzalobarrioss <49219316+Gonzalobarrioss@users.noreply.github.com>
2022-03-24 22:01:50 -0300

    update

--- frontend/components/MesaExamenInscriptasItem.js
+++ frontend/components/MesaExamenInscriptasItem.js
@@ -7,6 +7,7 @@ import { useSelector } from 'react-redux'
 import { store } from '../redux/store'
 
 import { isLoading } from '../redux/actions/LoadingAction'
+import moment from 'moment'
 
 
 const MesaExamen = ({ mesa }) => {
@@ -53,11 +54,40 @@ const MesaExamen = ({ mesa }) => {
         )
     }
 
+
+    const isValidDate = (fecha) => {
+
+        const fechaActual = moment().utcOffset('-03:00').format('YYYY-MM-DD')
+        const anioActual = parseInt(fechaActual.slice(0,4))
+        const mesActual = parseInt(fechaActual.slice(5,7))
+        const diaActual = parseInt(fechaActual.slice(8,10))
+
+        const fechaMesa = fecha.slice(0,10)
+        const anioMesa = parseInt(fechaMesa.slice(0,4))
+        const mesMesa = parseInt(fechaMesa.slice(5,7))
+        const diaMesa = parseInt(fechaMesa.slice(8,10))
+
+        if (anioActual == anioMesa){
+            if (mesActual == mesMesa){
+                if(diaActual <= diaMesa-2){
+                    return true
+                }
+                else{
+                    return false
+                }
+            }
+            else if(mesActual < mesMesa){
+                return true
+            }
+        }
+        
+    }
+
     return (
         <View style={styles.itemContainer}>
             <TouchableOpacity
                 onPress={ 
-                    () => bajaInscripcion(mesa.id)
+                    () => isValidDate(mesa.fecha) ?bajaInscripcion(mesa.id) : Alert.alert("La baja debe realizarse 48hs antes de la mesa")
                 }    
             >
                 <Text style={styles.itemDescripcion}>MESA: {mesa.descripcion}</Text>
